9a28205355d5751a0bbf20cfe99e197c8674be78
debroejm <debroejm@plu.edu>
2017-04-19 13:35:35 -0700

    Polygon Data Structure
    Created and tested a Polygon data structure that is comprised of a list of points and has a contains(Point) method.

--- core/src/main/java/plu/teamtwo/rtm/core/util/Polygon.java
+++ core/src/main/java/plu/teamtwo/rtm/core/util/Polygon.java
@@ -1,7 +1,81 @@
 package plu.teamtwo.rtm.core.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by MajorSlime on 4/12/2017.
+ * Polygon Utility Class. Used to represent a 2-dimensional shape comprised of multiple <code>Points</code>. Also
+ * contains helper methods for data checking, such as determining if a Point lies inside a Polygon.
  */
 public class Polygon {
+
+    public final Point[] points;
+    public final Point max;
+    public final Point min;
+
+    /**
+     * Array Constructor. Copies the contents of the given <code>points</code> array to this <code>Polygon's</code>
+     * internal data.
+     *
+     * @param points Array of <code>Points</code> to copy
+     */
+    public Polygon(Point[] points) {
+        if(points.length < 3) throw new IllegalArgumentException("Polygon must be instantiated with at least 3 Points");
+        this.points = Arrays.copyOf(points, points.length);
+        double xMin = Double.POSITIVE_INFINITY, yMin = Double.POSITIVE_INFINITY,
+                xMax = Double.NEGATIVE_INFINITY, yMax = Double.NEGATIVE_INFINITY;
+        for(Point p : points) {
+            double x = p.x.doubleValue(), y = p.y.doubleValue();
+            if(x < xMin) xMin = x; if(x > xMax) xMax = x;
+            if(y < yMin) yMin = y; if(y > yMax) yMax = y;
+        }
+        max = new Point(xMax, yMax);
+        min = new Point(xMin, yMin);
+    }
+
+    /**
+     * List Constructor. Turns the contents of the given <code>points List</code> into an array, and copies it to this
+     * <code>Polygon's</code> internal data.
+     *
+     * @param points <code>List</code> of <code>Points</code> to copy
+     */
+    public Polygon(List<Point> points) {
+        if(points.size() < 3) throw new IllegalArgumentException("Polygon must be instantiated with at least 3 Points");
+        this.points = points.toArray(new Point[]{});
+        double xMin = Double.POSITIVE_INFINITY, yMin = Double.POSITIVE_INFINITY,
+                xMax = Double.NEGATIVE_INFINITY, yMax = Double.NEGATIVE_INFINITY;
+        for(Point p : points) {
+            double x = p.x.doubleValue(), y = p.y.doubleValue();
+            if(x < xMin) xMin = x; if(x > xMax) xMax = x;
+            if(y < yMin) yMin = y; if(y > yMax) yMax = y;
+        }
+        max = new Point(xMax, yMax);
+        min = new Point(xMin, yMin);
+    }
+
+    /**
+     * Determines if this <code>Polygon</code> contains a <code>Point</code>.
+     *
+     * @param point <code>Point</code> to check
+     * @return <code>true</code> if the <code>Point</code> is inside this <code>Polygon</code>, <code>false</code> otherwise
+     */
+    public boolean contains(Point point) {
+        if( Point.inBounds(min, max, point)) {
+
+            Point extreme = new Point(Double.POSITIVE_INFINITY, point.y);
+
+            int count = 0;
+            for(int i = 0; i < points.length; i++) {
+
+                int next = (i+1) % points.length;
+                if(Point.lineIntersect(points[i], points[next], point, extreme)) {
+                    if(Point.orientation(points[i], point, points[next]) == Point.Orientation.COLINEAR)
+                        return Point.inBounds(points[i], points[next], point);
+                    else count++;
+                }
+            }
+
+            return count % 2 == 1;
+        } else return false;
+    }
 }
---
+++ core/src/test/java/plu/teamtwo/rtm/core/util/PolygonTest.java
@@ -0,0 +1,63 @@
+package plu.teamtwo.rtm.core.util;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class PolygonTest {
+
+    Point t0 = new Point(-8, -8);
+    Point t1 = new Point(0, 10);
+    Point t2 = new Point(8, -8);
+
+    @Test
+    public void construct() {
+        Polygon poly;
+
+        poly = new Polygon(new Point[]{ t0, t1, t2 });
+        assertEquals(t0, poly.points[0]);
+        assertEquals(t1, poly.points[1]);
+        assertEquals(t2, poly.points[2]);
+        assertEquals(new Point(-8, -8), poly.min);
+        assertEquals(new Point(8, 10), poly.max);
+
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(t0);
+        points.add(t1);
+        points.add(t2);
+
+        poly = new Polygon(points);
+        assertEquals(t0, poly.points[0]);
+        assertEquals(t1, poly.points[1]);
+        assertEquals(t2, poly.points[2]);
+        assertEquals(new Point(-8, -8), poly.min);
+        assertEquals(new Point(8, 10), poly.max);
+
+        try {
+            poly = new Polygon(new Point[]{ t0, t2 });
+            fail("Expected IllegalArgumentException when constructing Polygon with less than 3 points");
+        } catch(IllegalArgumentException ex) {}
+    }
+
+    @Test
+    public void contains() {
+        Polygon triangle = new Polygon(new Point[]{ t0, t1, t2 });
+
+        assertTrue(triangle.contains(new Point()));
+        assertTrue(triangle.contains(t0));
+        assertTrue(triangle.contains(t1));
+        assertTrue(triangle.contains(t2));
+
+        assertTrue(triangle.contains(new Point(-4, 1)));
+        assertTrue(triangle.contains(new Point(4, 1)));
+        assertTrue(triangle.contains(new Point(0, -8)));
+
+        assertFalse(triangle.contains(new Point(5, 5)));
+        assertFalse(triangle.contains(new Point(-5, 5)));
+        assertFalse(triangle.contains(new Point(-20, -4)));
+        assertFalse(triangle.contains(new Point(10, 10)));
+    }
+
+}
