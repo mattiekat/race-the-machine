95becb9a71925428e9d3f8f5e4c46df48112776d
Matthew Conover <matt@xram.co>
2017-04-27 12:19:25 -0700

    Wrote the innerworkings of the mutations tracker

--- neural-net/src/main/java/plu/teamtwo/rtm/neat/GenomeMutations.java
+++ neural-net/src/main/java/plu/teamtwo/rtm/neat/GenomeMutations.java
@@ -1,9 +1,14 @@
 package plu.teamtwo.rtm.neat;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Used to track mutations within a generation to properly give the same identification.
  */
-public class GenomeMutations {}
+public class GenomeMutations {
+}
 
 
 
@@ -11,34 +16,68 @@ public class GenomeMutations {}
  * Used to track mutations within a generation to properly give the same node ids and edge ids.
  */
 class DirectEncodingMutations extends GenomeMutations {
+    private Map<Integer, int[]> mutatedNodes = new HashMap<>();
+    private Map<Long, Integer> mutatedEdges = new HashMap<>();
+
 
     /**
      * Get the ID of a node mutated along an edge.
+     *
      * @param edge ID of the edge to check if a node was mutated on.
      * @return Array of [nodeID, edgeToID, EdgeFromID], or null if no node was mutated on that edge.
      */
     int[] getMutatedNodeID(int edge) {
-        return null;
+        int[] vals = mutatedNodes.get(edge);
+        return vals == null ? null : Arrays.copyOf(vals, 3);
     }
 
 
     /**
      * Get the ID (Innovation Number) of an edge which was mutated between the nodes `from` and `to`.
+     *
      * @param from Starting node of the edge to check for.
-     * @param to Ending node of the edge to check for.
+     * @param to   Ending node of the edge to check for.
      * @return ID of the added edge, or -1 if no edge was mutated between those nodes.
      */
     int getMutatedEdgeID(int from, int to) {
-        return -1;
+        Integer edge = mutatedEdges.get(hashTwoInts(from, to));
+        return edge == null ? -1 : edge;
     }
 
 
+    /**
+     * Add information about a newly mutated node.
+     * @param nodeID The ID of the new node.
+     * @param edgeToID ID of the edge going to the new node.
+     * @param edgeFromID ID of the edge coming from the new node.
+     * @param edge ID of the edge along which the node was added.
+     */
     void addMutatedNode(int nodeID, int edgeToID, int edgeFromID, int edge) {
-
+        mutatedNodes.put(edge, new int[]{nodeID, edgeToID, edgeFromID});
     }
 
 
+    /**
+     * Add information about a newly mutated edge.
+     * @param id ID of the new edge.
+     * @param from ID of the node which the edge comes from.
+     * @param to ID of the node which the edge goes to.
+     */
     void addMutatedEdge(int id, int from, int to) {
+        mutatedEdges.put(hashTwoInts(from, to), id);
+    }
 
+
+    /**
+     * Convert the (a, b) data to a unique id.
+     * @param a First value to be hashed.
+     * @param b Second value to be hashed.
+     * @return Unique, deterministic value defined by the two values.
+     */
+    private static long hashTwoInts(int a, int b) {
+        long result = a;
+        result <<= 32;
+        result |= b;
+        return result;
     }
 }
