a0b09d02852a22646d65de39f22f160ae1ab7635
Eadword <matt@xram.co>
2017-05-05 09:08:00 -0700

    Added comparisons to Edge and Node

--- neural-net/src/main/java/plu/teamtwo/rtm/neat/Edge.java
+++ neural-net/src/main/java/plu/teamtwo/rtm/neat/Edge.java
@@ -1,6 +1,6 @@
 package plu.teamtwo.rtm.neat;
 
-class Edge {
+class Edge implements Comparable<Edge> {
     /// The historically-based ID of this edge (innovation number)
     final int id;
     boolean enabled;
@@ -21,5 +21,23 @@ class Edge {
         this.fromNode = fromNode;
         this.id = id;
         this.weight = weight;
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Edge && compareTo((Edge) other) == 0;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+
+    @Override
+    public int compareTo(Edge other) {
+        return this.id - other.id;
     }
 }
--- neural-net/src/main/java/plu/teamtwo/rtm/neat/Node.java
+++ neural-net/src/main/java/plu/teamtwo/rtm/neat/Node.java
@@ -1,6 +1,6 @@
 package plu.teamtwo.rtm.neat;
 
-class Node {
+class Node implements Comparable<Node> {
     /// The historically-based ID of this Node
     public final int id;
     public NodeType nodeType;
@@ -19,5 +19,23 @@ class Node {
 
         this.id = id;
         this.nodeType = nodeType;
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Node && compareTo((Node) other) == 0;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+
+    @Override
+    public int compareTo(Node node) {
+        return this.id - node.id;
     }
 }
