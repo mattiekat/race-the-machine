fdc43c0236c5d878abe49cb1891a4833536c1d87
Eadword <matt@xram.co>
2017-05-14 19:05:45 -0700

    Tested Lunar Lander for segfaults without java

--- python/test.py
+++ python/test.py
@@ -1,14 +1,11 @@
 import gym
 
 if __name__ == '__main__':
-    env = gym.make('BipedalWalker-v2')
-    for i_episode in range(20):
+    env = gym.make('LunarLanderContinuous-v2')
+    for i_episode in range(10000):
         observation = env.reset()
-        for t in range(500):
+        done = False
+        while not done:
             env.render()
-            print(observation)
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
-            if done:
-                print("Episode finished after {} timesteps".format(t+1))
-                break
