05e4147c2e9c972e576f043e5a89d0028e451979
Eadword <matt@xram.co>
2017-05-17 18:30:07 -0700

    Made the OpenAI test interface generic

--- python/openai_gym_neat.py
+++ python/openai_gym_neat.py
@@ -1,13 +1,21 @@
 import gym
 import numpy
+from gym import spaces
 from py4j.java_gateway import JavaGateway, CallbackServerParameters
 
-INPUT_SIZE = 8
-OUTPUT_SIZE = 2
-WINNING_SCORE = 200.0
+# BipedalWalker-v2
+# BipedalWalkerHardcore-v2
+# CartPole-v1
+# LunarLander-v2
+# LunarLanderContinuous-v2
+# MountainCar-v0
+# MountainCarContinuous-v0
+# Pendulum-v0
+
+ENVIROMENT = 'BipedalWalkerHardcore-v2'
 
 
-class LunarLanderScore(object):
+class ScoreFunction(object):
     def __init__(self):
         self.done = False
         self.observation = env.reset()
@@ -15,7 +23,7 @@ class LunarLanderScore(object):
         self.steps = 0
 
     def createNew(self):
-        return LunarLanderScore()
+        return ScoreFunction()
 
     def getMaxThreads(self):
         return 1
@@ -34,10 +42,19 @@ class LunarLanderScore(object):
         return j_inputs
 
     def acceptOutput(self, output):
-        action = numpy.empty(2, dtype=numpy.float64)
-        for i in range(0, OUTPUT_SIZE):
-            val = numpy.float64(output[i])
-            action[i] = (val * 2.0) - 1.0
+        if DISCRETE:
+            action = None
+            max = -100.0
+            for i in range(0, OUTPUT_SIZE):
+                if numpy.float64(output[i]) > max:
+                    max = numpy.float64(output[i])
+                    action = i
+
+        else:
+            action = numpy.empty(OUTPUT_SIZE, dtype=numpy.float64)
+            for i in range(0, OUTPUT_SIZE):
+                val = numpy.float64(output[i])
+                action[i] = (val * 2.0) - 1.0
 
         self.observation, reward, self.done, _ = env.step(action)
         self.score += float(reward)
@@ -56,14 +73,26 @@ class LunarLanderScore(object):
 if __name__ == '__main__':
     gateway = JavaGateway(callback_server_parameters=CallbackServerParameters())
 
+    env = gym.make(ENVIROMENT)
+    INPUT_SIZE = env.observation_space.shape[0]
+    DISCRETE = isinstance(env.action_space, spaces.Discrete)
+
+    OUTPUT_SIZE = None
+    if DISCRETE:
+        OUTPUT_SIZE = env.action_space.n
+    else:
+        OUTPUT_SIZE = env.action_space.shape[0]
+
+    WINNING_SCORE = 1000.0 if env.spec.reward_threshold is None else env.spec.reward_threshold
+
+    print("Inputs: {}, Outputs: {}, Discrete: {}, Winning Score: {}".format(INPUT_SIZE, OUTPUT_SIZE, DISCRETE, WINNING_SCORE))
+
     gateway.entry_point.init(gateway.jvm.plu.teamtwo.rtm.genome.graph.GraphEncodingBuilder().inputs(INPUT_SIZE).outputs(OUTPUT_SIZE))
     controller = gateway.entry_point.getController()
     controller.createFirstGeneration()
 
-    env = gym.make('LunarLanderContinuous-v2')
-
     for _ in range(0, 500):
-        found_winner = controller.assesGeneration(LunarLanderScore())
+        found_winner = controller.assesGeneration(ScoreFunction())
         best = controller.getBestIndividual()
         print('Gen {:d}: {:.2f}, {:.1f}'
               .format(controller.getGenerationNum(), controller.getFitness(), best.getFitness()))
